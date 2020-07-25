ed18dda000c267f86d219f6aa809a5d304480451
meixin <meixinchoy@gmail.com>
2020-07-25 12:03:35 +0800

    2d platformer (up as jump)

--- main.asm/main.asm/main.asm
+++ main.asm/main.asm/main.asm
@@ -5,12 +5,170 @@ ExitProcess PROTO, dwExitCode: DWORD
 INCLUDE Irvine32.inc
 
 .data
-	;define variables here
+
+ground BYTE "------------------------------------------------------------------------------------------------------------------------",0
+
+strScore BYTE "Your score is: ",0
+score BYTE 0
+
+xPos BYTE 20
+yPos BYTE 20
+
+xCoinPos BYTE ?
+yCoinPos BYTE ?
+
+inputChar BYTE ?
 
 .code
 main PROC
-	;write assembly code here
+	; draw ground at (0,29):
+	mov dl,0
+	mov dh,29
+	call Gotoxy
+	mov edx,OFFSET ground
+	call WriteString
 
+	call DrawPlayer
+
+	call CreateRandomCoin
+	call DrawCoin
+
+	call Randomize
+
+	gameLoop:
+
+		; getting points:
+		mov bl,xPos
+		cmp bl,xCoinPos
+		jne notCollecting
+		mov bl,yPos
+		cmp bl,yCoinPos
+		jne notCollecting
+		; player is intersecting coin:
+		inc score
+		call CreateRandomCoin
+		call DrawCoin
+		notCollecting:
+
+		mov eax,white (black * 16)
+		call SetTextColor
+
+		; draw score:
+		mov dl,0
+		mov dh,0
+		call Gotoxy
+		mov edx,OFFSET strScore
+		call WriteString
+		mov al,score
+		call WriteInt
+
+		; gravity logic:
+		gravity:
+		cmp yPos,27
+		jg onGround
+		; make player fall:
+		call UpdatePlayer
+		inc yPos
+		call DrawPlayer
+		mov eax,80
+		call Delay
+		jmp gravity
+		onGround:
+
+		; get user key input:
+		call ReadChar
+		mov inputChar,al
+
+		; exit game if user types 'x':
+		cmp inputChar,"x"
+		je exitGame
+
+		cmp inputChar,"w"
+		je moveUp
+
+		cmp inputChar,"s"
+		je moveDown
+
+		cmp inputChar,"a"
+		je moveLeft
+
+		cmp inputChar,"d"
+		je moveRight
+
+		moveUp:
+		; allow player to jump:
+		mov ecx,1
+		jumpLoop:
+			call UpdatePlayer
+			dec yPos
+			call DrawPlayer
+			mov eax,70
+			call Delay
+		loop jumpLoop
+		jmp gameLoop
+
+		moveDown:
+		call UpdatePlayer
+		inc yPos
+		call DrawPlayer
+		jmp gameLoop
+
+		moveLeft:
+		call UpdatePlayer
+		dec xPos
+		call DrawPlayer
+		jmp gameLoop
+
+		moveRight:
+		call UpdatePlayer
+		inc xPos
+		call DrawPlayer
+		jmp gameLoop
+
+	jmp gameLoop
+
+	exitGame:
+	exit
 INVOKE ExitProcess,0
 main ENDP
+
+DrawPlayer PROC
+	; draw player at (xPos,yPos):
+	mov dl,xPos
+	mov dh,yPos
+	call Gotoxy
+	mov al,"X"
+	call WriteChar
+	ret
+DrawPlayer ENDP
+
+UpdatePlayer PROC
+	mov dl,xPos
+	mov dh,yPos
+	call Gotoxy
+	mov al," "
+	call WriteChar
+	ret
+UpdatePlayer ENDP
+
+DrawCoin PROC
+	mov eax,yellow (yellow * 16)
+	call SetTextColor
+	mov dl,xCoinPos
+	mov dh,yCoinPos
+	call Gotoxy
+	mov al,"X"
+	call WriteChar
+	ret
+DrawCoin ENDP
+
+CreateRandomCoin PROC
+	mov eax,55
+	inc eax
+	call RandomRange
+	mov xCoinPos,al
+	mov yCoinPos,27
+	ret
+CreateRandomCoin ENDP
+
 END main
