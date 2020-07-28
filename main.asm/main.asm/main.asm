dacdcf76b393cb2a1386a185e93d3f1710384a99
meixin <meixinchoy@gmail.com>
2020-07-28 17:25:51 +0800

    added some comments and did some spacing adj (same w dev now)

--- main.asm/main.asm/main.asm
+++ main.asm/main.asm/main.asm
@@ -21,7 +21,7 @@ yCoinPos BYTE ?
 
 inputChar BYTE ?
 
-speed		WORD 0
+speed	WORD 0
 
 StartFlag BYTE 1			;1 means that the program has just started, 0 means otherwise
 
@@ -44,7 +44,7 @@ loop L1
 	call CreateRandomCoin
 	call DrawCoin
 
-	call Randomize
+	call Randomize				;set up finish
 
 	gameLoop:
 
@@ -56,50 +56,53 @@ loop L1
 		mov bl,yPos[0]
 		cmp bl,yCoinPos
 		jne notCollecting
-		; player is intersecting coin:
+
+		; snake is eating coin:
 		inc score
 		mov ebx, 1
 		add bl, score
-		mov snake[ebx], "x"
+		mov snake[ebx], "x"		;add one unit to the snake
 		mov ah, yPos[ebx-1]
-		mov al, xPos[ebx-1]
+		mov al, xPos[ebx-1]	
 		mov xPos[ebx], al
-		mov yPos[ebx], ah
+		mov yPos[ebx], ah		;pos of new tail = pos of old tail
 
-		cmp xPos[ebx-2], al
-		jne checky
+		cmp xPos[ebx-2], al		;check if the old tail and the unit before is on the yAxis
+		jne checky				;jump if not on the yAxis
 
-		cmp yPos[ebx-2], ah
-		jl incy
+		cmp yPos[ebx-2], ah		;check if the new tail should be above or below of the old tail 
+		jl incy			
 		jg decy
-		incy:
+		incy:					;inc if below
 		inc yPos[ebx]
 		jmp continue
-		decy:
+		decy:					;dec if above
 		dec yPos[ebx]
 		jmp continue
 
-		checky:
-		cmp yPos[ebx-2], ah
+		checky:					;old tail and the unit before is on the xAxis
+		cmp yPos[ebx-2], ah		;check if the new tail should be right or left of the old tail
 		jl incx
 		jg decx
-		incx:
-		inc xPos[ebx]
+		incx:					;inc if right
+		inc xPos[ebx]			
 		jmp continue
-		decx:
+		decx:					;dec if left
 		dec xPos[ebx]
 		jmp continue
 
+		;update game
 		continue:
 		call DrawPlayer
 		call CreateRandomCoin
-		call DrawCoin
+		call DrawCoin			
+
 		notCollecting:
 
 		mov eax,white (black * 16)
 		call SetTextColor
 
-		; draw score:
+		; draw score
 		mov dl,0
 		mov dh,0
 		call Gotoxy
@@ -108,17 +111,16 @@ loop L1
 		mov al,score
 		call WriteInt		
 
-		; get user key input:
+		; get user key input
 		cmp StartFlag, 1
 		je Initialinput
 		call ReadKey
-            jz noKey   
+            jz noKey		;jump if no key is entered
 		processInput:
 		mov inputChar,al
 
 		noKey:
-		; exit game if user types 'x':
-		cmp inputChar,"x"
+		cmp inputChar,"x"	
 		je exitGame
 
 		cmp inputChar,"w"
@@ -139,49 +141,46 @@ loop L1
 		jne moveDown
 		jmp gameLoop
 
-		checkLeft:	;snake cant go too far over to the left
+		checkLeft:		;snake cant go too far over to the left
 		cmp xPos[0],1
 		jne moveLeft
 		jmp gameLoop
 
-		checkRight:	;snake cant go too far over to the right
+		checkRight:		;snake cant go too far over to the right
 		cmp xPos[0],118
 		jne moveRight
 		jmp gameLoop
 
-		checkTop:	;snake cant go too far over to the top
+		checkTop:		;snake cant go too far over to the top
 		cmp yPos,1
 		jne moveUp
 		jmp gameLoop
 
-
-		moveUp:
-		call delayfunc
+		
+		moveUp:			
+		call delayfunc		;slow down the moving
 		mov ecx, 1
-		add cl, score
-		mov ebx, 0
-		call UpdatePlayer
-		mov ah, yPos[ebx]
-		mov al, xPos[ebx]
-		dec yPos[ebx]
-		call DrawPlayer
+		add cl, score		;number of iterations to print the snake body n tail
+		mov ebx, 0			;index 0(snake head)
+		call UpdatePlayer	
+		mov ah, yPos[ebx]	
+		mov al, xPos[ebx]	;alah stores the pos of the snake's next unit 
+		dec yPos[ebx]		;move the head up
+		call DrawPlayer		
 	L5:	
-		inc ebx
+		inc ebx				;loop to print remaining units of snake
 		call UpdatePlayer
 		mov dl, xPos[ebx]
-		mov dh, yPos[ebx]
+		mov dh, yPos[ebx]	;dldh temporarily stores the current pos of the unit 
 		mov yPos[ebx], ah
-		mov xPos[ebx], al
+		mov xPos[ebx], al	;assign new position to the unit
 		mov al, dl
-		mov ah,dh
+		mov ah,dh			;move the current position back into alah
 		call DrawPlayer
 	loop L5
 		jne gameLoop
 
 		
-
-
-
 		moveDown:
 		call delayfunc
 		mov ecx, 1
@@ -204,6 +203,7 @@ loop L1
 		call DrawPlayer
 	loop L4
 		jmp gameLoop
+
 
 		moveLeft:
 		call delayfunc
@@ -255,25 +255,25 @@ loop L1
 
 jmp gameLoop
 
-Initialinput:
-	call readChar
+Initialinput:			
+	call readChar		;bc program will glitch if use readKey for the first input
 	mov StartFlag, 0
 	jmp processInput
 
-	exitGame::
+	exitGame:
 	exit
 INVOKE ExitProcess,0
 main ENDP
 
 DrawPlayer PROC
-	; draw player at (xPos,yPos):
+	; draw player at (xPos,yPos)
 	mov dl,xPos[ebx]
 	mov dh,yPos[ebx]
 	call Gotoxy
-	mov dl, al
-	mov al, snake[ebx]
+	mov dl, al			;temporarily save al in dl
+	mov al, snake[ebx]		
 	call WriteChar
-	mov al, dl
+	mov al, dl			
 	ret
 DrawPlayer ENDP
 
@@ -281,7 +281,7 @@ UpdatePlayer PROC
 	mov dl, xPos[ebx]
 	mov dh,yPos[ebx]
 	call Gotoxy
-	mov dl, al
+	mov dl, al			;temporarily save al in dl
 	mov al, " "
 	call WriteChar
 	mov al, dl
@@ -301,28 +301,28 @@ DrawCoin ENDP
 
 CreateRandomCoin PROC
 	mov eax,118
-	call RandomRange
-	inc eax
+	call RandomRange	;0-117
+	inc eax				;1-118
 	mov xCoinPos,al
 	mov eax,28
-	call RandomRange
-	inc eax
+	call RandomRange	;0-27
+	inc eax				;1-28
 	mov yCoinPos,al
 	ret
 CreateRandomCoin ENDP
 
-; start delay
-delayfunc PROC
-mov bx, 3500
-mov cx, 3500
-delay2:
-dec bx
-cmp bx,0 
-jne delay2
-dec cx
-cmp cx,0    
-jne delay2
-ret
+
+delayfunc PROC			;loops to slow down the prog
+	mov bx, 3500
+	mov cx, 3500
+	delay2:
+	dec bx
+	cmp bx,0 
+	jne delay2
+	dec cx
+	cmp cx,0   
+	jne delay2
+	ret
 delayfunc ENDP
 
 END main
